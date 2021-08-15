cfe927d7d3503c8dd987df0ba5444e0406dfee41
Akash Yadav <81186992+AK-aShH@users.noreply.github.com>
2021-08-15 06:10:58 +0530

    Create Leetcode1.cpp file
    Given a binary array nums, return the maximum number of consecutive 1's in the array.

---
+++ Arrays/Leetcode1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int c=0,max=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==1)
+                c++;
+                if(nums[i]==0||i==nums.size()-1)
+            {
+                if(c>max)
+                    max=c;
+                c=0;
+            }
+        }return max;
+    }
+};
